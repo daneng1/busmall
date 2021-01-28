e6b5524e6d5124d0091c2ca99d032877299325aa
daneng1 <engeldb@gmail.com>
2021-01-27 16:13:13 -0800

    updated to local storage, fixed bug in image views

--- app.js
+++ app.js
@@ -8,35 +8,40 @@ function ProductImage(image, alt) {
   this.timesShown = 0;
   this.image = image;
   this.alt = alt;
+  this.flag = false;
 
   ProductImage.allImages.push(this);
 }
 // Creates our allImages property on the ProductImages constructor
-ProductImage.allImages = [];
+var productsFromLocal = localStorage.getItem('products');
+var productsAsArray = JSON.parse(productsFromLocal);
+
+ProductImage.allImages = productsAsArray || [];
 
 // creates the product image
-new ProductImage('./images/bag.jpg', 'Bag');
-new ProductImage('./images/banana.jpg', 'Banana');
-new ProductImage('./images/bathroom.jpg', 'Bathroom');
-new ProductImage('./images/boots.jpg', 'Boots');
-new ProductImage('./images/breakfast.jpg', 'Breakfast');
-new ProductImage('./images/bubblegum.jpg', 'Bubblegum');
-new ProductImage('./images/chair.jpg', 'Chair');
-new ProductImage('./images/cthulhu.jpg', 'Cthulhu');
-new ProductImage('./images/dog-duck.jpg', 'Dog-duck');
-new ProductImage('./images/dragon.jpg', 'Dragon');
-new ProductImage('./images/pen.jpg', 'Pen');
-new ProductImage('./images/pet-sweep.jpg', 'Pet-sweep');
-new ProductImage('./images/scissors.jpg', 'Scissors');
-new ProductImage('./images/shark.jpg', 'Shark');
-new ProductImage('./images/sweep.png', 'Sweep');
-new ProductImage('./images/tauntaun.jpg', 'Tauntaun');
-new ProductImage('./images/unicorn.jpg', 'Unicorn');
-new ProductImage('./images/usb.gif', 'Usb');
-new ProductImage('./images/water-can.jpg', 'Water-can');
-new ProductImage('./images/wine-glass.jpg', 'Wine-glass');
-
-console.log(ProductImage.allImages);
+if (ProductImage.allImages.length === 0) {
+  new ProductImage('./images/bag.jpg', 'Bag');
+  new ProductImage('./images/banana.jpg', 'Banana');
+  new ProductImage('./images/bathroom.jpg', 'Bathroom');
+  new ProductImage('./images/boots.jpg', 'Boots');
+  new ProductImage('./images/breakfast.jpg', 'Breakfast');
+  new ProductImage('./images/bubblegum.jpg', 'Bubblegum');
+  new ProductImage('./images/chair.jpg', 'Chair');
+  new ProductImage('./images/cthulhu.jpg', 'Cthulhu');
+  new ProductImage('./images/dog-duck.jpg', 'Dog-duck');
+  new ProductImage('./images/dragon.jpg', 'Dragon');
+  new ProductImage('./images/pen.jpg', 'Pen');
+  new ProductImage('./images/pet-sweep.jpg', 'Pet-sweep');
+  new ProductImage('./images/scissors.jpg', 'Scissors');
+  new ProductImage('./images/shark.jpg', 'Shark');
+  new ProductImage('./images/sweep.png', 'Sweep');
+  new ProductImage('./images/tauntaun.jpg', 'Tauntaun');
+  new ProductImage('./images/unicorn.jpg', 'Unicorn');
+  new ProductImage('./images/usb.gif', 'Usb');
+  new ProductImage('./images/water-can.jpg', 'Water-can');
+  new ProductImage('./images/wine-glass.jpg', 'Wine-glass');
+}
+// console.log(ProductImage.allImages);
 
 var imageContainer = document.getElementById('image-container');
 var leftImage = document.getElementById('left-image');
@@ -49,13 +54,23 @@ function generateRandomImages() {
   var leftIndex = Math.floor(Math.random() * ProductImage.allImages.length);
   var centerIndex = Math.floor(Math.random() * ProductImage.allImages.length);
   var rightIndex = Math.floor(Math.random() * ProductImage.allImages.length);
-
-  while (centerIndex === leftIndex) {
+  console.log(`Index #'s are ${leftIndex}, ${centerIndex}, ${rightIndex}`);
+  while (ProductImage.allImages[leftIndex].flag === true) {
+    leftIndex = Math.floor(Math.random() * ProductImage.allImages.length);
+  }
+  while (centerIndex === leftIndex || ProductImage.allImages[centerIndex].flag === true) {
     centerIndex = Math.floor(Math.random() * ProductImage.allImages.length);
   }
-  while (rightIndex === leftIndex || rightIndex === centerIndex) {
+  while (rightIndex === leftIndex || ProductImage.allImages[rightIndex].flag === true || rightIndex === centerIndex || ProductImage.allImages[rightIndex].flag === true) {
     rightIndex = Math.floor(Math.random() * ProductImage.allImages.length);
   }
+  console.log(ProductImage.allImages[leftIndex], ProductImage.allImages[centerIndex], ProductImage.allImages[rightIndex]);
+  for (var i=0; i<ProductImage.allImages.length; i++) {
+    ProductImage.allImages[i].flag = false;
+  }
+  ProductImage.allImages[leftIndex].flag = true;
+  ProductImage.allImages[centerIndex].flag = true;
+  ProductImage.allImages[rightIndex].flag = true;
 
   var leftProduct = ProductImage.allImages[leftIndex];
   var centerProduct = ProductImage.allImages[centerIndex];
@@ -64,24 +79,9 @@ function generateRandomImages() {
   return [leftProduct, centerProduct, rightProduct];
 }
 
+// render new images and ensure that there are no duplicates from last round
 function renderImages(leftProduct, centerProduct, rightProduct) {
-  var currentlyRenederedImages = [leftProduct.alt, centerProduct.alt, rightProduct.alt];
 
-  var newRenderedImages = generateRandomImages();
-
-  while (
-    currentlyRenederedImages[0] === newRenderedImages[0].alt ||
-    currentlyRenederedImages[0] === newRenderedImages[1].alt ||
-    currentlyRenederedImages[0] === newRenderedImages[2].alt ||
-    currentlyRenederedImages[1] === newRenderedImages[0].alt ||
-    currentlyRenederedImages[1] === newRenderedImages[1].alt ||
-    currentlyRenederedImages[1] === newRenderedImages[2].alt ||
-    currentlyRenederedImages[2] === newRenderedImages[0].alt ||
-    currentlyRenederedImages[2] === newRenderedImages[1].alt ||
-    currentlyRenederedImages[2] === newRenderedImages[2].alt
-  ) {
-    newRenderedImages = generateRandomImages();
-  }
   leftImage.src = leftProduct.image;
   leftImage.alt = leftProduct.alt;
   leftProduct.timesShown++;
@@ -101,20 +101,19 @@ renderImages(randomImages[0], randomImages[1], randomImages[2]);
 var numberOfClicks = 0;
 var numberOfRounds = 25;
 
-
+// iterates the number of rounds and stops when limit reached
 imageContainer.addEventListener('click', function (event) {
   console.log(event.target);
   for (var i=0; i < ProductImage.allImages.length; i++){
     if (event.target.alt === (ProductImage.allImages[i].alt)){
       ProductImage.allImages[i].timesClicked++;
       numberOfClicks++;
-      console.log(ProductImage.allImages[i].alt);
     }
   }
   if (numberOfClicks < numberOfRounds) {
     var randomProducts = generateRandomImages();
     renderImages(randomProducts[0], randomProducts[1], randomProducts[2]);
-    console.log(numberOfClicks);
+    // console.log(numberOfClicks);
   }
   else{
     imageContainer.style.display='none';
@@ -122,33 +121,36 @@ imageContainer.addEventListener('click', function (event) {
     var headerMain = document.getElementById('headerMain');
     headerMain.style.display='none';
     return resultButton;
-
   }
-  var newImages = generateRandomImages();
-  renderImages(newImages[0], newImages[1], newImages[2]);
 });
 
 var votesByProduct = [];
 var timesProductsAreShown = [];
 var productLabels = [];
 
+// displays chart after button click
 resultButton.addEventListener('click', function (event) {
   console.log(event.target);
+  event.preventDefault();
 
   for (var i=0; i < ProductImage.allImages.length; i++) {
     votesByProduct.push(ProductImage.allImages[i].timesClicked);
     timesProductsAreShown.push(ProductImage.allImages[i].timesShown);
     productLabels.push(ProductImage.allImages[i].alt);
   }
-  console.log(votesByProduct);
-  console.log(timesProductsAreShown);
-  console.log(productLabels);
+  console.log(`Votes are ${votesByProduct}`);
+  console.log(`Shown are ${timesProductsAreShown}`);
+  // console.log(productLabels);
+
+  var productsAsString = JSON.stringify(ProductImage.allImages);
+  console.log(productsAsString);
+  localStorage.setItem('products', productsAsString);
 
   resultButton.style.display='none';
   var header = document.getElementById('header');
   var myChart = document.getElementById('myText');
   header.style.display='flex';
-  myChart.style.display='block';
+  myChart.style.display='flex';
   return header;
 });
 
@@ -163,123 +165,28 @@ var myChart = new Chart(ctx, {
     datasets: [{
       label: '# of Times Seen',
       data: timesProductsAreShown,
-      backgroundColor: [
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)',
-        'rgba(141, 28, 28, 01.0)'
-      ],
-      borderColor: [
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)',
-        'rgba(88, 28, 28, 01.0)'
-      ],
+      backgroundColor:  new Array(20).fill('rgba(141, 28, 28, 01.0)'),
+      borderColor: new Array(20).fill('rgba(88, 28, 28, 01.0)'),
       borderWidth: 1
     },
     {
       label: '# of Votes',
       data:  votesByProduct,
-      backgroundColor: [
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)',
-        'rgba(49, 132, 190, 1.0)'
-      ],
-      borderColor: [
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)',
-        'rgba(32, 100, 160, 1.0)'
-      ],
+      backgroundColor: new Array(20).fill('rgba(49, 132, 190, 1.0)'),
+      borderColor: new Array(20).fill('rgba(32, 100, 160, 1.0)'),
       borderWidth: 1
     }]
   },
   options: {
+    animation: {
+      tension: {
+        duration: 1000,
+        easing: 'linear',
+        from: 1,
+        to: 0,
+        loop: true
+      }
+    },
     scales: {
       yAxes: [{
         ticks: {
